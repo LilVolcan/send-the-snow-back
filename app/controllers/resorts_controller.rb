8af9b878531c126622c800c582bba09634ccc995
github username <github email address>
2019-11-21 18:05:02 -0500

    finished backend mvp

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -8,10 +8,10 @@ class ResortsController < ApplicationController
 
     def resort_filter
         resorts = Resort.all.select{ |resort| resort[:state_id] == params[:state_id].to_i}
-        byebug
         sorted_resorts = resorts.sort{|a,b| b.conditions[0][:past_24_hour].to_i <=> a.conditions[0][:past_24_hour].to_i} 
-       
+        top_five = sorted_resorts.slice(0,5) 
 
+        render json: ResortSerializer.new(top_five).serialized_json
     end 
 
 
